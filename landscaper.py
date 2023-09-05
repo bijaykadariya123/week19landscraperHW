f0b19b00e032dc48304ce2b31d1965ee0ec7cdd5
Bijay Kadariya <bijaykadariya92@gmail.com>
2023-09-05 17:56:39 -0400

    landscaper: user can buy scissors

--- landscaper.py
+++ landscaper.py
@@ -12,9 +12,9 @@ def mow():
         money += 1
         print(money)                            # testing:
 
-    # elif current_tool=="Rusty Scissors":
-    #     money += 5
-    #     print(money)
+    elif current_tool=="Rusty Scissors":
+        money += 5
+        print(money)
         
     # elif tools=="Push Mower":
     #     money += 50
@@ -34,14 +34,39 @@ def start_mowing():
         return "buy"
     else:
         return "wrong input"
+
+def upgrade_tools():
+    global money
+    global current_tool
+    buying_new_tool = int(input("What tool do you want to buy, 1. Rusty Scissors, 2. Push Mower, 3. Fancy Mower, 4. Starving Students : "))
+    list_of_tools=["Rusty Scissors", "Push Mower", "Fancy Mower", "Starving Students"]
+    # if list_of_tools[0]:
+    #     money-=5
     
+    if buying_new_tool == 1 and money > 5:
+        current_tool = "Rusty Scissors"
+        
+    # if buying_new_tool == "Rusty Scissors":
+    #     money -= 5
+    # elif buying_new_tool == "Push Mower":
+    #     money -= 50
+    # elif buying_new_tool == "Fancy Mower":
+    #     money -= 100
+    # elif buying_new_tool == "Starving Students":
+    #     money -= 250
+    # else:
+    #     "invalid Input"
+
 
 def user_choice(choice):
     if choice=="mow":
         mow()
+    elif choice == "buy":
+        upgrade_tools()
 
 
 # start_mowing()
 while money<1000:
     mowing_value=start_mowing()
     user_choice(mowing_value)
+    # upgrade_tools()
